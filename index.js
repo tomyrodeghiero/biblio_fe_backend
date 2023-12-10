dabd3a8d3dd05685ab87e5f69c9b1fba1829c542
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-12-10 15:07:53 -0300

    Update backend

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ app.post("/api/users", async (req, res) => {
   }
 });
 
-app.get("/api/users", async (req, res) => {
+app.get("/api/get-users", async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).send(users);
